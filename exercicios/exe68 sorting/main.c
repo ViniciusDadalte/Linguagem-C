7b05e6adde238e124d7a014791f75571b94fc005
ViniciusDadalte <viniciusdadalte745@gmail.com>
2024-07-31 19:40:02 -0300

    Criei o exercicio 68 para poder testar algoritimos de ordenção, comecei criando uma função para o bubble sort, e aproveitei e criei uma função para imprimir o array para poder testar o algoritimo

---
+++ exercicios/exe68 sorting/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printArray(int arr[], int max);
+void bubbleSort(int arr[], int max);
+
+int main(void)
+{
+    system("cls");
+
+    int array[5] = {15, 16, 6, 8, 5};
+
+    printf("Before: ");
+    printArray(array, 5);
+
+    bubbleSort(array, 5);
+    
+    printf("After: ");
+    printArray(array, 5);
+
+    return 0;
+}
+
+void printArray(int arr[], int max)
+{
+    for (int i = 0; i <= max - 1; i++)
+    {
+        printf("%i ", arr[i]);
+    }
+    printf("\n");
+}
+
+void bubbleSort(int arr[], int max)
+{
+    int temp;
+     
+    for (int i = 0; i < max - 1; i++)
+    {
+        for (int j = 0; j < max - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
